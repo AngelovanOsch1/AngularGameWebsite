ed3f881cab315b5832587f489769832818f396e3
Angelo van Osch <Angelo.van.osch@hotmail.com>
2023-12-27 18:04:42 +0100

    search function

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,12 +1,52 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../interfaces/interfaces';
+import { RepositoryService } from '../services/repository.service';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss'],
+  providers: [RepositoryService],
 })
 export class LandingComponent {
+  userObservable: Observable<User[]> | undefined;
+  usersList: User[] = [];
+
+  filteredUsersList: { profilePhoto: string; username: string }[] = [];
+
   activeIndex: number = 1;
+
+  constructor(private repositoryService: RepositoryService) {
+    this.userObservable = this.repositoryService.usersCollection.valueChanges();
+  }
+  ngOnInit() {
+    this.userObservable?.subscribe((UserDoc: User[]) => {
+      this.usersList = UserDoc;
+    });
+
+    this.searchForm
+      .get('search')!
+      .valueChanges.subscribe((searchQuery: string) => {
+        if (searchQuery.trim() === '') {
+          this.filteredUsersList = [];
+        } else {
+          this.filteredUsersList = this.usersList
+            .filter((user) => {
+              return user.username
+                .toLowerCase()
+                .includes(searchQuery.toLowerCase());
+            })
+            .map(({ profilePhoto, username }) => ({ profilePhoto, username }));
+        }
+      });
+  }
+
+  searchForm: FormGroup = new FormGroup({
+    search: new FormControl(''),
+  });
+
   setActive(index: number) {
     this.activeIndex = index;
   }
