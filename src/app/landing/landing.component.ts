170a3012b4bc52789defca12080266b56fac890b
Angelo van Osch <Angelo.van.osch@hotmail.com>
2023-12-27 15:15:33 +0100

    changed FAQ logic

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -6,18 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./landing.component.scss'],
 })
 export class LandingComponent {
-  activeIndex: number | null = null;
-  ngOnInit(): void {
-    // Set the first item as active initially
-    this.activeIndex = 0;
-  }
-  setActive(index: number): void {
-    if (this.activeIndex === index) {
-      // Clicked on the active item again, remove the active state
-      this.activeIndex = null;
-    } else {
-      // Clicked on a different item, toggle the active state
-      this.activeIndex = index;
-    }
+  activeIndex: number = 1;
+  setActive(index: number) {
+    this.activeIndex = index;
   }
 }
