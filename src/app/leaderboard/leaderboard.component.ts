cfe6910e057a59ec4261af046aff57ecabcda1de
Brendan Kersten <mistrbrend@gmail.com>
2024-01-04 21:37:44 +0100

    start of shop page

--- src/app/leaderboard/leaderboard.component.ts
+++ src/app/leaderboard/leaderboard.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-leaderboard',
   templateUrl: './leaderboard.component.html',
-  styleUrls: ['./leaderboard.component.sass']
+  styleUrls: ['./leaderboard.component.scss']
 })
 export class LeaderboardComponent {
 
