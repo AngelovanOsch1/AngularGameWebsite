706d12442859021181cb428315d4d45810684cd0
Angelo van Osch <Angelo.van.osch@hotmail.com>
2024-03-10 22:25:53 +0100

    shopping cart logic

--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -3,6 +3,8 @@ import { map, tap } from 'rxjs';
 import { ShoppingCart, User } from '../interfaces/interfaces';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { UserAuthService } from '../services/user-auth-service.service';
+import { FormGroup, FormControl } from '@angular/forms';
+import { increment } from 'firebase/firestore';
 
 @Component({
   selector: 'app-shopping-cart',
@@ -13,6 +15,8 @@ import { UserAuthService } from '../services/user-auth-service.service';
 export class ShoppingCartComponent implements OnInit {
   user: User | undefined;
   shoppingCartList: ShoppingCart[] = [];
+  shoppingCartFormGroups: FormGroup[] = [];
+  totalItems: number = 0;
 
   constructor(
     private firestore: AngularFirestore,
@@ -34,8 +38,51 @@ export class ShoppingCartComponent implements OnInit {
         ),
         tap((shoppingCart: ShoppingCart[]) => {
           this.shoppingCartList = shoppingCart;
+          this.shoppingCartFormGroups = [];
+          this.totalItems = shoppingCart.reduce(
+            (acc, item) => acc + item.quantity,
+            0
+          );
+          this.shoppingCartList.forEach((shoppingCartItem) => {
+            const shoppingCartForm = new FormGroup({
+              quantity: new FormControl(shoppingCartItem.quantity),
+            });
+            this.shoppingCartFormGroups.push(shoppingCartForm);
+          });
         })
       )
       .subscribe();
+  }
+
+  shoppingCartForm: FormGroup = new FormGroup({
+    quantity: new FormControl(),
+  });
+
+  decrementQuantity(shoppingCartItem: ShoppingCart) {
+    this.firestore
+      .collection(`users/${this.user!.id}/shoppingcart`)
+      .doc(shoppingCartItem.id)
+      .update({
+        quantity: increment(-1),
+      });
+  }
+
+  incrementQuantity(shoppingCartItem: ShoppingCart) {
+    this.firestore
+      .collection(`users/${this.user!.id}/shoppingcart`)
+      .doc(shoppingCartItem.id)
+      .update({
+        quantity: increment(1),
+      });
+  }
+  inputQuantity(shoppingCartItem: ShoppingCart, i: number) {
+    const quantityValue = this.shoppingCartFormGroups[i].get('quantity')!.value;
+
+    this.firestore
+      .collection(`users/${this.user!.id}/shoppingcart`)
+      .doc(shoppingCartItem.id)
+      .update({
+        quantity: quantityValue,
+      });
   }
 }
